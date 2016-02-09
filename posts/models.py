eaff5eafcf0982747a072cd26a29342de1d8b277
Michal Čihař <michal@cihar.com>
2016-02-09 17:43:05 +0100

    Initial data model
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,51 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from markupfield.fields import MarkupField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name=_('name'),
+        max_length=100
+    )
+    slug = models.SlugField(
+        verbose_name=_('URL slug'),
+        db_index=True,
+        unique_for_date='date'
+    )
+    description = MarkupField(
+        verbose_name=_('Description'),
+        default_markup_type='markdown'
+    )
+
+
+class Entry(models.Model):
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=100
+    )
+    slug = models.SlugField(
+        verbose_name=_('URL slug'),
+        db_index=True,
+        unique_for_date='date'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name=_('Category'),
+    )
+    date = models.DateTimeField(
+        verbose_name=_('Date'),
+        default=timezone.now,
+        db_index=True
+    )
+    summary = MarkupField(
+        verbose_name=_('Summary'),
+        default_markup_type='markdown'
+    )
+    body = MarkupField(
+        verbose_name=_('Text'),
+        default_markup_type='markdown'
+    )
