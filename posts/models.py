29f9cdc15175aa43bde1535c258888388bbce4ee
Michal Čihař <michal@cihar.com>
2018-02-21 14:00:34 +0100

    Modernize posts to Django 2.0
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
@@ -8,7 +6,7 @@ from markupfield.fields import MarkupField
 
 class Category(models.Model):
     name = models.CharField(
-        verbose_name=_('name'),
+        verbose_name=_('Name'),
         max_length=100
     )
     slug = models.SlugField(
@@ -35,6 +33,7 @@ class Entry(models.Model):
     category = models.ForeignKey(
         Category,
         verbose_name=_('Category'),
+        on_delete=models.deletion.CASCADE,
     )
     date = models.DateTimeField(
         verbose_name=_('Date'),
