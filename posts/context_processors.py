c694b0b6da4c2e07c4aecba070b671baad8a769d
Michal Čihař <michal@cihar.com>
2018-02-22 21:01:15 +0100

    Fix class name
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 
-from .models import Category, Post
+from .models import Category, Entry
 
 
 def context(request):
     return {
         'categories': Category.objects.all(),
-        'og_post': Post.objects.filter(pk=settings.OG_POST),
+        'og_entry': Entry.objects.filter(pk=settings.OG_POST),
     }
