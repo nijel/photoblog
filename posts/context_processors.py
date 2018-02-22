2974d9d3bc0e91e9b5b6bd141a93822b56645f82
Michal Čihař <michal@cihar.com>
2018-02-22 21:02:02 +0100

    Return only single object, not a list
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -6,5 +6,5 @@ from .models import Category, Entry
 def context(request):
     return {
         'categories': Category.objects.all(),
-        'og_entry': Entry.objects.filter(pk=settings.OG_POST),
+        'og_entry': Entry.objects.get(pk=settings.OG_POST),
     }
