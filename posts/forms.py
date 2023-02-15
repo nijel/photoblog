fb306b263e900a2fef002403ac7fc4b285866bae
Michal Čihař <michal@cihar.com>
2023-02-15 11:18:50 +0100

    Avoid using deprecated ugettext

--- posts/forms.py
+++ posts/forms.py
@@ -3,7 +3,7 @@ from captcha.widgets import ReCaptchaV3
 from django import forms
 from django.conf import settings
 from django.core.mail import send_mail
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from markupfield.fields import MarkupField
 from versatileimagefield.fields import PPOIField, VersatileImageField
 
